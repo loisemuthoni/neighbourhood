ee6b18c5e12b07976ce8389a30a985d46dbb03b3
Moringa School <loisemburu01@gmail.com>
2020-02-25 16:50:04 +0300

    added a base html and created a function for the login

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def homepage(request):
+
+    businesses=Business.get_all_businesses()
+    neighborhoods=NeighborHood.get_all_neighborhoods()
+    posts=Post.get_all_posts()
+
+    current_user=request.user
+    if request.method == 'POST':
+        form =NewCommentForm(request.POST,request.FILES)
+        if form.is_valid():
+            comment=form.save(commit=False)
+
+            comment.commenter = current_user
+
+            comment.save()
+        return redirect('homepage')
+    else:
+        form=NewCommentForm()
+
+
+    return render(request,'homepage.html',{"businesses":businesses,"neighborhoods":neighborhoods,"posts":posts,"form":form})
+
