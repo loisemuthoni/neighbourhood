dc13f4580a0f5a88dab2f06b1095fe397d8ee956
Moringa School <loisemburu01@gmail.com>
2020-02-26 08:47:42 +0300

    registered the admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import NeighborHood,Userprofile,Business,Post,PoliceCenters,HealthCenter
 
 # Register your models here.
+
+admin.site.register(NeighborHood)
+admin.site.register(Userprofile)
+admin.site.register(Business)
+admin.site.register(Post)
+
+admin.site.register(PoliceCenters)
+admin.site.register(HealthCenter)
