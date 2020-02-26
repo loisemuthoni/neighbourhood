bd08fa8e98bf26ee4a824b29320fb287bca1f62b
Moringa School <loisemburu01@gmail.com>
2020-02-26 10:12:28 +0300

    created a database

--- neigborHood/settings.py
+++ neigborHood/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'neigborHood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
+        'USER':'moringa',
+        'PASSWORD':'lilo1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
