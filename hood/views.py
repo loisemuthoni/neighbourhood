8e62e9cc5b6f338532ffa80d640b7682a5a1686e
Moringa School <loisemburu01@gmail.com>
2020-02-26 09:17:38 +0300

    created a search function in the views

--- hood/views.py
+++ hood/views.py
@@ -81,3 +81,24 @@ def business(request):
     else:
         form=NewBusinessForm()
     return render(request,'business.html',{"form":form})
+
+
+def search_results(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_businesses = Business.search_by_business_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"businesses": searched_businesses})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+def businessdetails(request,business_id):
+
+    business=Business.objects.get(id=business_id)
+
+    return render(request,"businessdetails.html",{"business":business})
