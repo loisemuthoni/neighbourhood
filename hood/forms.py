e22e248e0eef991c375c58bc6f9930474df8f960
Moringa School <loisemburu01@gmail.com>
2020-02-26 08:49:09 +0300

    made a form for the newbusiness and updateform

--- hood/forms.py
+++ hood/forms.py
@@ -6,3 +6,13 @@ class NewProfileForm(forms.ModelForm):
     class Meta:
         model=Userprofile
         exclude=['user_name']
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['user']
+
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model=Userprofile
+        exclude=["profile_image","user_name","email"]
