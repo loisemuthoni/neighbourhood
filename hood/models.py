4f5bac8aa7d67db0e83bf22e9d3ff7bf3bced895
Moringa School <loisemburu01@gmail.com>
2020-02-26 08:52:51 +0300

    made a comment class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post,blank=True, on_delete=models.CASCADE,null=True,related_name='comment')
+    commenter=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    comment=models.TextField(max_length =30)
+
+
+
+    def delete_comment(self):
+        self.delete()
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_comments(cls):
+        comments=cls.objects.all()
+        return comments
+
+    @classmethod
+    def get_comments_by_post_id(cls,post_id):
+        comments=cls.objects.filter(id=post_id)
+        return comments
